1aab99ccf96e3cb9f89af8542d331e0025a18beb
kangchih <kangchihkuo@gmail.com>
2014-03-23 04:39:52 -0700

    uncomment soundmananger.h

--- Tank/include/manager/SoundManager.h
+++ Tank/include/manager/SoundManager.h
@@ -1,22 +1,22 @@
-//#ifndef __SoundManager_h_
-//#define __SoundManager_h_
-//
-//namespace OgreOggSound {
-//	class OgreOggSoundManager;
-//}
-//
-//class SoundManager {
-//public:
-//	SoundManager();
-//	~SoundManager();
-//	void playFireSound();
-//	void playExplosionSound();
-//
-//private:
-//	void createFireSound();
-//	void createExplosionSound();
-//
-//	OgreOggSound::OgreOggSoundManager *mOggSoundManager;
-//};
-//
-//#endif
+#ifndef __SoundManager_h_
+#define __SoundManager_h_
+
+namespace OgreOggSound {
+	class OgreOggSoundManager;
+}
+
+class SoundManager {
+public:
+	SoundManager();
+	~SoundManager();
+	void playFireSound();
+	void playExplosionSound();
+
+private:
+	void createFireSound();
+	void createExplosionSound();
+
+	OgreOggSound::OgreOggSoundManager *mOggSoundManager;
+};
+
+#endif
